ccf62c0803a2cf70806ce7d124a97c66386376e3
Niklaus48 <145317214+Niklaus48@users.noreply.github.com>
2024-01-29 12:39:42 +0330

    EP32

--- Controller/notes/show.php
+++ Controller/notes/show.php
@@ -7,9 +7,22 @@ $config = require base_path('config.php');
 
 $db = new DataBase($config['database']);
 
-$heading = 'My Notes';
-
 $id = $_GET['id'];
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+    $note = $db -> query('select *from notes where id = :id',['id' => $id])->findOrFail();
+
+    $currentUserId = 1;
+
+    authoriz($note['user_id'] === $currentUserId,Response::FORBIDEN);
+
+    $db -> query('delete from notes where id = :id',[
+        ':id' => $id,
+    ]);
+
+    header('location: /notes');
+}
 
 $note = $db -> query('select *from notes where id = :id',['id' => $id])->findOrFail();
 
--- view/notes/show.view.php
+++ view/notes/show.view.php
@@ -7,6 +7,11 @@
             <a href="/notes" class="text-blue-500 hover:underline"> go Back... </a>
 
               <p><?= htmlspecialchars($note['body']) ?></p>
+
+            <form method="post">
+                <input type="hidden" name="id" value=<?= $note['id'] ?>>
+                <button type="submit" name="id" class="text-red-500 mt-6 hover:underline"> delete </button>
+            </form>
         </div>
     </main>
 </div>
